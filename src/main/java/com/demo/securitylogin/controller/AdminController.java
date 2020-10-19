cc0007df086a5f67d814d98855bdb944945d49c1
YYGang <ddcgang@qq.com>
2020-10-19 13:30:25 +0800

    swagger

--- src/main/java/com/demo/securitylogin/controller/AdminController.java
+++ src/main/java/com/demo/securitylogin/controller/AdminController.java
@@ -1,5 +1,9 @@
 package com.demo.securitylogin.controller;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.User;
@@ -9,8 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/")
+@Api(tags = "Admin-管理员后台")
 public class AdminController extends BaseController {
     @RequestMapping("/")
+    @ApiOperation("列表")
     public String index(ModelMap model) {
         User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
         request.getSession().setAttribute("username", user.getUsername());
--- src/main/java/com/demo/securitylogin/controller/sys/UserController.java
+++ src/main/java/com/demo/securitylogin/controller/sys/UserController.java
@@ -3,6 +3,10 @@ package com.demo.securitylogin.controller.sys;
 import com.demo.securitylogin.model.sys.User;
 import com.demo.securitylogin.model.util.ResultUtil;
 import com.demo.securitylogin.service.sys.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("sys/user")
+@Api(tags = "人员接口",description="人员文档说明",hidden=true)
 public class UserController {
     @Autowired
     UserService userService;
@@ -21,7 +26,11 @@ public class UserController {
 
     @GetMapping("find")
     @ResponseBody
-    @PreAuthorize("hasPermission('/sys/user/find','r')")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "userName",value = "账号",required = true,dataType = "String")
+    })
+    @ApiOperation("根据账号查询用户信息")
+    //@PreAuthorize("hasPermission('/sys/user/find','r')")
     public ResultUtil find(String userName) {
         return new ResultUtil(userService.findByName(userName));
     }
